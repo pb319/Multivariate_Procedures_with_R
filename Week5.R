f7cae5adb6b2d3474438c3f90c964d3db47419d2
Pranay Biswas <66114329+pb319@users.noreply.github.com>
2024-04-27 20:46:33 +0530

    Update Week5.R

--- Week5.R
+++ Week5.R
@@ -1,3 +1,4 @@
+# 3d Plot
 install.packages("scatterplot3d")
 library(scatterplot3d)
 H =c(100,120,150,176,180)
@@ -8,17 +9,32 @@ data3d[1:3,]
 scatterplot3d(data3d[1:3,])
 help("scatterplot3d")
 stars(data3d,labels = c("1","2","3","4","5"))
-max(data3d,axis=1)
-help(stars)
-help(max)
-for(i in x)
-  print(i/max(data3d))
+
+#Star Plot
+Height =c(100,120,150,176,180)
+Weight = c(28,35,55,74,85)
+Age = c(5,8,15,18,25)
+data3d=data.frame(Height, Weight, Age)
+stars(data3d)
+
+# Chernoff Faces
+install.packages("aplpack")
+library(aplpack)
+faces(data3d)
+
 
 # Normal Distribution
 
 # P(X>22)
 1- pnorm(q=22,mean=20,sd=sqrt(4))
 pnorm(q=22,mean = 20, sd = sqrt(4), lower.tail = F)
+
+# 90% quantile q
+?qnorm
+qnorm(p=0.9, mean=20, sd=sqrt(4))
+
+# generate 4 random numbers
+rnorm(4,mean=20,sd=sqrt(4))
 
 # Assignment 5
 x=c(33,31,36,39,40)
